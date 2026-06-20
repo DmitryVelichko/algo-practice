a9aac4efb2997c3151933bd2be69db3ebcf533be
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-20 14:35:54 +0300

    Algorithms practice

--- arrays and hashing/242. Valid Anagram.py
+++ arrays and hashing/242. Valid Anagram.py
@@ -1,3 +1,35 @@
+# 242. Valid Anagram
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "rat", t = "car"
+
+# Output: false
+
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+ 
+
+# Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
 # O(n), O(26 english letters) => O(1)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
@@ -13,3 +45,4 @@ class Solution:
             if i != 0:
                 return False
         return True
+    
