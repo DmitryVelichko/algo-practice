88c0b3cb09e9829d1b4aaa4126d731f0aef0c00e
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-21 22:16:47 +0300

    Algorithms practice

--- arrays and hashing/347. Top k frequent elements.py
+++ arrays and hashing/347. Top k frequent elements.py
@@ -48,8 +48,8 @@ class Solution:
         for num in nums:
             count[num] = 1 + count.get(num, 0)
         # [[],[],[],["1"]]
-        for num, cnt in count.items():
-            freq[cnt].append(num)
+        for key in count:
+            freq[count[key]].append(key)
 
         res = []
         for i in range(len(freq) - 1, 0, -1):
