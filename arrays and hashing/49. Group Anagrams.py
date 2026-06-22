e8502aec54707faa54e616959a7312fe131d2ad1
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-22 15:57:24 +0300

    Algorithms practice

--- arrays and hashing/49. Group Anagrams.py
+++ arrays and hashing/49. Group Anagrams.py
@@ -1,3 +1,36 @@
+
+
+# Example 1:
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+# Explanation:
+
+# There is no string in strs that can be rearranged to form "bat".
+# The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+# The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+# Example 2:
+
+# Input: strs = [""]
+
+# Output: [[""]]
+
+# Example 3:
+
+# Input: strs = ["a"]
+
+# Output: [["a"]]
+
+ 
+
+# Constraints:
+
+# 1 <= strs.length <= 104
+# 0 <= strs[i].length <= 100
+# strs[i] consists of lowercase English letters.
+
 # O(n, k), n - number of strs, k - len(str), O(n*k) - hasmap of arrays
 # Create [] with 26 zeros, each zero++, {010101: []}
 class Solution:
