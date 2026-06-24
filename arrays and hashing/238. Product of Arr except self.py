953a2af2de728886f56d058cf454212cf1724447
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-24 11:46:07 +0300

    Algorithms practice

---
+++ arrays and hashing/238. Product of Arr except self.py
@@ -0,0 +1,14 @@
+# O(n), O(n) if output array does not count as extra space - O(1)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
