f14769b873bab70d78145f41bd0fcb18068e4a3b
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-25 15:27:05 +0300

    Algorithms practice

---
+++ arrays and hashing/128. Longest Consecutive Sequence.py
@@ -0,0 +1,12 @@
+# 128. Longest Consecutive Sequence
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+# You must write an algorithm that runs in O(n) time.
+
+ 
+
