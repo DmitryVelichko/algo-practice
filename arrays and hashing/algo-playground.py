a8307a573058c8ce5dbed6fbe4b0fd55b7ae9937
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-28 09:58:46 +0300

    Algorithms practice

--- arrays and hashing/algo-playground.py
+++ arrays and hashing/algo-playground.py
@@ -51,4 +51,24 @@ class Solution:
             res[tuple(count)].append(s)
         return list(res.values())
     
+    # O(n), O(26 english letters) => O(1)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = {}
+        for i in range(len(s)):
+            count[s[i]] = count.get(s[i], 0) + 1
+            count[t[i]] = count.get(t[i], 0) - 1
+        
+        for i in count:
+            if count[i] != 0:
+                return False
+        return True
     
+
+    # Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+    #  The biggest issue with Unicode in anagram problems is that the same visual character can be represented in multiple ways in memory.
+     # Normalize both strings to NFC format (canonical composition)
+        # s = unicodedata.normalize("NFC", s) in memory
