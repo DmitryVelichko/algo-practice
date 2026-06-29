1c5dfeadc95593d86ad13a8a7226d3fc3941af66
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-29 10:32:30 +0300

    Algorithms practice

--- arrays and hashing/algo-playground.py
+++ arrays and hashing/algo-playground.py
@@ -128,3 +128,21 @@ class Solution:
             res.append(s[j + 1 : j + 1 + length])
             i = j + 1 + length
         return res
+
+# 128. Longest Consecutive Sequence
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+# You must write an algorithm that runs in O(n) time.
+
+ 
+
+# Example 1:
+
+# Input: nums = [100,4,200,1,3,2]
+# Output: 4
+# Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
