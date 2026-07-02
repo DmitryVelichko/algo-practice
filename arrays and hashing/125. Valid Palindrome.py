2f52bbc06052630d14e3bfea179ab1decd2206e4
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-02 12:36:56 +0300

    Algorithms practice

--- arrays and hashing/125. Valid Palindrome.py
+++ arrays and hashing/125. Valid Palindrome.py
@@ -32,3 +32,26 @@
 
 # 1 <= s.length <= 2 * 105
 # s consists only of printable ASCII characters.
+
+# 2 pointers, alpha numeric function
+# O(n), O(1)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]): l += 1
+            while l < r and not self.alphaNum(s[r]): r -= 1
+
+            if s[l].lower() != s[r].lower(): return False
+
+            l += 1
+            r -= 1
+
+        return True
+
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
