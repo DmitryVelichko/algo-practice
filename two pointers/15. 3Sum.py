8f450ddfd7142ed550a4dff6e98f1faba33194b5
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-04 11:20:01 +0300

    Algorithms practice

--- two pointers/15. 3Sum.py
+++ two pointers/15. 3Sum.py
@@ -37,3 +37,24 @@
 
 # 3 <= nums.length <= 3000
 # -105 <= nums[i] <= 105
+
+# Sort array, skip duplicates, threeSum + 2 pointers, skip duplicates
+# O(n^2), O(n) coz of Timsort
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if i>0 and nums[i] == nums[i-1]: continue
+            l = i+1
+            r = len(nums) - 1
+            while(l<r):
+                threeSum = nums[i] + nums[l] + nums[r]
+                if threeSum > 0: r -= 1
+                elif threeSum < 0: l+=1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l+=1
+                    r-=1
+                    while l<r and nums[l] == nums[l-1]: l += 1
+        return res
