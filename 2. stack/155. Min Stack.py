9393cc9785ab53bed453fd80ee954fbf40c4b279
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-10 11:41:24 +0300

    leetcode daily grind

--- 2. stack/155. Min Stack.py
+++ 2. stack/155. Min Stack.py
@@ -18,3 +18,28 @@
 
  
 
+# Example 1:
+
+# Input
+# ["MinStack","push","push","push","getMin","pop","top","getMin"]
+# [[],[-2],[0],[-3],[],[],[],[]]
+
+# Output
+# [null,null,null,null,-3,null,0,-2]
+
+# Explanation
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin(); // return -3
+# minStack.pop();
+# minStack.top();    // return 0
+# minStack.getMin(); // return -2
+ 
+
+# Constraints:
+
+# -231 <= val <= 231 - 1
+# Methods pop, top and getMin operations will always be called on non-empty stacks.
+# At most 3 * 104 calls will be made to push, pop, top, and getMin.
