c49c6bc66acc6baebdbef51c1f9060e20d89d4f1
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-10 11:41:36 +0300

    leetcode daily grind

--- 2. stack/155. Min Stack.py
+++ 2. stack/155. Min Stack.py
@@ -43,3 +43,27 @@
 # -231 <= val <= 231 - 1
 # Methods pop, top and getMin operations will always be called on non-empty stacks.
 # At most 3 * 104 calls will be made to push, pop, top, and getMin.
+
+# 2 stacks, push() - if minStack is not empty append the min value from 2 stacks
+# O(1), O(n)
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, value: int) -> None:
+        self.stack.append(value)
+        if self.minStack:
+            value = min(value, self.minStack[-1])
+        self.minStack.append(value)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else None
+
+    def getMin(self) -> int:
+        return self.minStack[-1] if self.minStack else None
+
