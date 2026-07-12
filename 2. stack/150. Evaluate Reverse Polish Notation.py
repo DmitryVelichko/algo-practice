b6616a627ede8c6371d00a2cf080c3052a5e606f
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-12 11:12:39 +0300

    leetcode daily grind

--- 2. stack/150. Evaluate Reverse Polish Notation.py
+++ 2. stack/150. Evaluate Reverse Polish Notation.py
@@ -45,3 +45,23 @@
 
 # 1 <= tokens.length <= 104
 # tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+# Stack, if "+-/*" pop 2 values from stack and perform calculation, if not append to stack 
+# O(n), O(n)
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
