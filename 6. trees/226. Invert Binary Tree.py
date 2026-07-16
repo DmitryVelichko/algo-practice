f52c2ce5c75d28ac2f66a23824cdf7f21490a6a6
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-16 21:44:53 +0300

    leetcode daily grind

--- 6. trees/226. Invert Binary Tree.py
+++ 6. trees/226. Invert Binary Tree.py
@@ -28,3 +28,21 @@
 
 # The number of nodes in the tree is in the range [0, 100].
 # -100 <= Node.val <= 100
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# DFS recursive pre-order: Time: O(n), Space: O(h), h = height (maximum depth) of the tree 
+# Balanced tree: O(log n), Skewed tree (linked list): O(n)
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+        
