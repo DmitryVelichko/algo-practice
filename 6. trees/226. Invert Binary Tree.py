3f5a3aec310fde921ad1f19a612bf27d11c9777e
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-16 21:44:57 +0300

    leetcode daily grind

--- 6. trees/226. Invert Binary Tree.py
+++ 6. trees/226. Invert Binary Tree.py
@@ -46,3 +46,14 @@ class Solution:
         self.invertTree(root.right)
         return root
         
+# DFS iterative with stack
+    def invertTreeIterative(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left: stack.append(node.left)
+            if node.right: stack.append(node.right)
+        return root
+        
