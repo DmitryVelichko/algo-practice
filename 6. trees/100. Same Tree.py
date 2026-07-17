1dd91f13e5367ed3c33804f31a0d03fa9ee65b13
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-17 12:08:05 +0300

    leetcode daily grind

--- 6. trees/100. Same Tree.py
+++ 6. trees/100. Same Tree.py
@@ -31,3 +31,18 @@
 
 # The number of nodes in both trees is in the range [0, 100].
 # -104 <= Node.val <= 104
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# DFS pre-order, O(n), O(h): best: O(log n), worst: O(n)
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q: return True
+        if not p or not q or p.val != q.val: return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
