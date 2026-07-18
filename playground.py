827a31806393d7e5bb7ff83d03fbee8545f6c427
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-18 09:52:30 +0300

    leetcode daily grind

--- playground.py
+++ playground.py
@@ -65,3 +65,62 @@ class Solution:
             else:
                 stack.append(int(c))
         return stack[0]
+    
+    # 100. Same Tree
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+ 
+
+# Example 1:
+
+
+# Input: p = [1,2,3], q = [1,2,3]
+# Output: true
+# Example 2:
+
+
+# Input: p = [1,2], q = [1,null,2]
+# Output: false
+# Example 3:
+
+
+# Input: p = [1,2,1], q = [1,1,2]
+# Output: false
+ 
+
+# Constraints:
+
+# The number of nodes in both trees is in the range [0, 100].
+# -104 <= Node.val <= 104
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# DFS pre-order, O(n), O(h): best: O(log n), worst: O(n)
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q: return True
+        if not p or not q or p.val != q.val: return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# DFS iterative
+def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        stack = [(p, q)]
+        while stack:
+            node1, node2 = stack.pop()
+            if not node1 and not node2: continue
+            if not node1 or not node2 or node1.val != node2.val: return False
+            stack.append((node1.right, node2.right))
+            stack.append((node1.left, node2.left))
+        return True
