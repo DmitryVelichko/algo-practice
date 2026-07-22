7b4908bf578e7d4d627d613a34e72bef2db8dd7d
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-22 12:15:30 +0300

    leetcode daily grind

--- 6. trees/572. Subtree of Another Tree.py
+++ 6. trees/572. Subtree of Another Tree.py
@@ -29,3 +29,24 @@
 # The number of nodes in the subRoot tree is in the range [1, 1000].
 # -104 <= root.val <= 104
 # -104 <= subRoot.val <= 104
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#
+# DFS recursive with 2 edge cases + DFS recursive helper function isSameTree() 
+# O(m * n), O(m + n), m and n - number of nodes in main tree and subtree
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot: return True
+        if not root: return False
+        if self.isSameTree(root, subRoot): return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSameTree(self, p, q) -> bool:
+        if not p and not q: return True
+        if not p or not q or p.val != q.val: return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
