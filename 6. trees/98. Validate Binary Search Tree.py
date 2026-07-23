9584bcf70fee4f9d9352697c823dacd603248327
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-23 11:25:47 +0300

    leetcode daily grind

--- 6. trees/98. Validate Binary Search Tree.py
+++ 6. trees/98. Validate Binary Search Tree.py
@@ -34,3 +34,10 @@
 # DFS, check if node's value is in boundaries (-∞, +∞), left < node < right,
 # going left → tighten right boundary, going right → tighten left boundary
 # O(n), O(h)
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(node, left, right):
+            if not node: return True
+            if not (left < node.val < right): return False
+            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
+        return valid(root, float("-inf"), float("inf"))
