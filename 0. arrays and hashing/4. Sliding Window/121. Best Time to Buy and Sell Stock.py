6193497b04c69165c9e63bfeb13ee3b07773094b
Dmitry Velichko <soleil-2005@mail.ru>
2026-07-26 10:12:09 +0300

    leetcode daily grind

--- 0. arrays and hashing/4. Sliding Window/121. Best Time to Buy and Sell Stock.py
+++ 0. arrays and hashing/4. Sliding Window/121. Best Time to Buy and Sell Stock.py
@@ -11,3 +11,13 @@
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
  
+# Sliding window, itertate and update minPrice and maxProfit (current price - minPrice)
+# O(n), O(1)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minPrice = prices[0] 
+        maxProfit = 0
+        for price in prices:
+            minPrice = min(minPrice, price)
+            maxProfit = max(maxProfit, price - minPrice)
+        return maxProfit
