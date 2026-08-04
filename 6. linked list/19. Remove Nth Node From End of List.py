aa75aa59e23400f016a93aba3c02d8ca2ba79fe5
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-04 10:57:19 +0300

    leetcode daily grind

---
+++ 6. linked list/19. Remove Nth Node From End of List.py
@@ -0,0 +1,11 @@
+# 19. Remove Nth Node From End of List
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+ 
+
