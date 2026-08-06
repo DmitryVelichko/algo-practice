849295f5ff07f9865d80b80df440d958b04dd3f8
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-06 11:22:47 +0300

    leetcode daily grind

--- 6. linked list/138. Copy List with Random Pointer.py
+++ 6. linked list/138. Copy List with Random Pointer.py
@@ -20,3 +20,26 @@
 
  
 
+# Example 1:
+
+
+# Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+# Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+# Example 2:
+
+
+# Input: head = [[1,1],[2,1]]
+# Output: [[1,1],[2,1]]
+# Example 3:
+
+
+
+# Input: head = [[3,null],[3,0],[3,null]]
+# Output: [[3,null],[3,0],[3,null]]
+ 
+
+# Constraints:
+
+# 0 <= n <= 1000
+# -104 <= Node.val <= 104
+# Node.random is null or is pointing to some node in the linked list.
