f38859b0e89939cc6cb96d26a6b9dc40df881953
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-07 11:56:47 +0300

    leetcode daily grind

--- 6. linked list/143. Reorder List.py
+++ 6. linked list/143. Reorder List.py
@@ -14,3 +14,19 @@
 
  
 
+# Example 1:
+
+
+# Input: head = [1,2,3,4]
+# Output: [1,4,2,3]
+# Example 2:
+
+
+# Input: head = [1,2,3,4,5]
+# Output: [1,5,2,4,3]
+ 
+
+# Constraints:
+
+# The number of nodes in the list is in the range [1, 5 * 104].
+# 1 <= Node.val <= 1000
