9435da798a9669385af1aa8ab93fac05c8a064ef
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-13 16:05:38 +0300

    leetcode daily grind

--- 7. trees/playground.py
+++ 7. trees/playground.py
@@ -50,3 +50,63 @@ class Solution:
         if not p and not q: return True
         if not p or not q or p.val != q.val: return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    # 226. Invert Binary Tree
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [4,2,7,1,3,6,9]
+# Output: [4,7,2,9,6,3,1]
+# Example 2:
+
+
+# Input: root = [2,1,3]
+# Output: [2,3,1]
+# Example 3:
+
+# Input: root = []
+# Output: []
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# DFS recursive pre-order: Time: O(n), Space: O(h), h = height (maximum depth) of the tree 
+# Balanced tree: O(log n), Skewed tree (linked list): O(n)
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+        
+# DFS iterative with stack
+    def invertTreeIterative(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left: stack.append(node.left)
+            if node.right: stack.append(node.right)
+        return root
+        
