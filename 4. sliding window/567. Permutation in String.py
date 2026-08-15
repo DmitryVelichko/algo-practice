87aa073e374db92473e5d312465ba7c2d9409311
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-15 10:00:48 +0300

    leetcode daily grind

--- 4. sliding window/567. Permutation in String.py
+++ 4. sliding window/567. Permutation in String.py
@@ -26,3 +26,34 @@
 
 # 1 <= s1.length, s2.length <= 104
 # s1 and s2 consist of lowercase English letters.
+
+# Sliding window, 2 frequency arrays. Count chars for s1 and the current s2 window. Track the number of matching chars, update it when adding/removing chars. matches == 26 → permutation found.
+# O(n), O(1)
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2): return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26: return True
+
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]: matches += 1
+            elif s1Count[index] + 1 == s2Count[index]: matches -= 1
+
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]: matches += 1
+            elif s1Count[index] - 1 == s2Count[index]: matches -= 1
+            l += 1
+        return matches == 26
