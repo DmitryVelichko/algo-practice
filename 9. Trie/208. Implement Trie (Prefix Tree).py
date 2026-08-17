de1f75aef3a9375ae54149a7551f9457b3569128
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-17 13:50:39 +0300

    leetcode daily grind

--- 9. Trie/208. Implement Trie (Prefix Tree).py
+++ 9. Trie/208. Implement Trie (Prefix Tree).py
@@ -13,3 +13,26 @@
 # boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
  
 
+# Example 1:
+
+# Input
+# ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+# [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+# Output
+# [null, null, true, false, true, null, true]
+
+# Explanation
+# Trie trie = new Trie();
+# trie.insert("apple");
+# trie.search("apple");   // return True
+# trie.search("app");     // return False
+# trie.startsWith("app"); // return True
+# trie.insert("app");
+# trie.search("app");     // return True
+ 
+
+# Constraints:
+
+# 1 <= word.length, prefix.length <= 2000
+# word and prefix consist only of lowercase English letters.
+# At most 3 * 104 calls in total will be made to insert, search, and startsWith.
