076681191055cc91eb55677633cad941e2a3c5b3
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-20 12:30:37 +0300

    leetcode daily grind

--- 10. Heap/703. Kth Largest Element in a Stream.py
+++ 10. Heap/703. Kth Largest Element in a Stream.py
@@ -56,3 +56,22 @@
 # -104 <= val <= 104
 # At most 104 calls will be made to add.
 
+# Min Heap
+# Time: O(n log k), Space: O(k)
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
