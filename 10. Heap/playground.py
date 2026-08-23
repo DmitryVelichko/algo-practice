4a27bbb75fd65c2ee4746cb43305b9e27281e47d
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-23 23:36:48 +0300

    leetcode daily grind

--- 10. Heap/playground.py
+++ 10. Heap/playground.py
@@ -26,3 +26,32 @@
 
 # 1 <= k <= nums.length <= 105
 # -104 <= nums[i] <= 104
+
+# Sorting: O(n log n), O(n)/O(1) depending on sorting algo
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return nums[len(nums) - k]
+# Min-heap: O(n log k), O(k)
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        while k < len(nums):
+            heapq.heappop(nums)
+        return nums[0]
+# *Best: Quick Select (Time: O(n) on average, O(n^2) worst case, Space: O(n))
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+            if p > k:
+                return quickSelect(l, p - 1)
+            elif p < k:
+                return quickSelect(p + 1, r)
+            else:
+                return nums[p]
+        return quickSelect(0, len(nums) - 1)
