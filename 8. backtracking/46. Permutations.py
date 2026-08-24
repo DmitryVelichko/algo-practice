8b71f61a695deb1dd8dd86626f7b69e721662132
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-24 12:14:35 +0300

    leetcode daily grind

--- 8. backtracking/46. Permutations.py
+++ 8. backtracking/46. Permutations.py
@@ -27,3 +27,19 @@
 # 1 <= nums.length <= 6
 # -10 <= nums[i] <= 10
 # All the integers of nums are unique.
+
+# Iterative: O(!n * n^2), O(!n * n)
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+
+        perms = self.permute(nums[1:])
+        res = []
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+        return res
+
