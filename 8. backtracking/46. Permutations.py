6d71dee6d2d5c2b13f45ada01fd4b978f00bf968
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-24 12:14:23 +0300

    leetcode daily grind

---
+++ 8. backtracking/46. Permutations.py
@@ -0,0 +1,10 @@
+# 46. Permutations
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
