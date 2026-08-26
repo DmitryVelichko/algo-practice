3c0274be9927a654124f96c97fb53c9cfa28d8da
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-26 12:12:13 +0300

    leetcode daily grind

--- 8. backtracking/131. Palindrome Partitioning.py
+++ 8. backtracking/131. Palindrome Partitioning.py
@@ -22,3 +22,29 @@
 
 # 1 <= s.length <= 16
 # s contains only lowercase English letters.
+
+# Time: O(n*2^n), Space: O(n) extra space, O(n*2^n) space for the output list
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res, part = [], []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    part.append(s[i : j + 1])
+                    dfs(j + 1)
+                    part.pop()
+
+        dfs(0)
+        return res
+
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
