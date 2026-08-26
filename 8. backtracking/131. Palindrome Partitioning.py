9cdcc76009550ff089e67c4b8cd5acea08e60601
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-26 12:12:06 +0300

    leetcode daily grind

---
+++ 8. backtracking/131. Palindrome Partitioning.py
@@ -0,0 +1,9 @@
+# 131. Palindrome Partitioning
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+ 
