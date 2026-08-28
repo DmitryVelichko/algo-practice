5f8dbf2d8f2baff8d3d2ed9c47bd183ccf4e78ad
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-28 12:50:37 +0300

    leetcode daily grind

--- 11. graph/200. Number of Islands.py
+++ 11. graph/200. Number of Islands.py
@@ -36,3 +36,28 @@
 # n == grid[i].length
 # 1 <= m, n <= 300
 # grid[i][j] is '0' or '1'.
+
+# DFS. Time: O(r*c), Space: O(r*c), r = number of rows, c = number of columns
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= ROWS or
+                c >= COLS or grid[r][c] == "0"
+            ):
+                return
+
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands += 1
+
+        return islands
