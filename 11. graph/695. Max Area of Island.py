34993e22333336dc416f490fb850080c3bb1834f
Dmitry Velichko <soleil-2005@mail.ru>
2026-08-29 13:25:44 +0300

    leetcode daily grind

--- 11. graph/695. Max Area of Island.py
+++ 11. graph/695. Max Area of Island.py
@@ -12,3 +12,44 @@
 
  
 
+# Example 1:
+
+
+# Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+# Output: 6
+# Explanation: The answer is not 11, because the island must be connected 4-directionally.
+# Example 2:
+
+# Input: grid = [[0,0,0,0,0,0,0,0]]
+# Output: 0
+ 
+
+# Constraints:
+
+# m == grid.length
+# n == grid[i].length
+# 1 <= m, n <= 50
+# grid[i][j] is either 0 or 1.
+
+# DFS
+# O(r*c), O(r*c), r, c = number of rows, columns
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if (r < 0 or r == ROWS or c < 0 or
+                c == COLS or grid[r][c] == 0 or
+                (r, c) in visit): return 0
+            visit.add((r, c))
+            return (1 + dfs(r + 1, c) +
+                        dfs(r - 1, c) +
+                        dfs(r, c + 1) +
+                        dfs(r, c - 1))
+
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r, c))
+        return area
