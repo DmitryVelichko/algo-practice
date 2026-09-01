ccdaf0d7f1cb7659ec6e6d75974cd853265b73b9
Dmitry Velichko <soleil-2005@mail.ru>
2026-09-01 10:48:25 +0300

    leetcode daily grind

---
+++ 13. intervals/meeting rooms.py
@@ -0,0 +1,8 @@
+# Meeting Rooms
+# Easy
+# Topics
+# Company Tags
+# Hints
+# Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts. The intervals may be provided in any order.
+
+# Note: (0,8),(8,10) is not considered a conflict at 8
