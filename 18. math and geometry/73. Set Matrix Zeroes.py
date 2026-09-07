96cdd013ab1e71ae440f7e415f6b56ceb84b8d4d
Dmitry Velichko <soleil-2005@mail.ru>
2026-09-07 10:56:31 +0300

    leetcode daily grind

--- 18. math and geometry/73. Set Matrix Zeroes.py
+++ 18. math and geometry/73. Set Matrix Zeroes.py
@@ -36,3 +36,31 @@
 # A straightforward solution using O(mn) space is probably a bad idea.
 # A simple improvement uses O(m + n) space, but still not the best solution.
 # Could you devise a constant space solution?
+
+# O(m*n), O(1)
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0
