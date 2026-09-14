610a9feaac2dbf228351283b7b84e0bf06819ca3
Dmitry Velichko <soleil-2005@mail.ru>
2026-09-14 11:12:53 +0300

    leetcode daily grind

--- 0. Arrays and Hashing/14. Longest Common Prefix.go
+++ 0. Arrays and Hashing/14. Longest Common Prefix.go
@@ -25,3 +25,15 @@
 // strs[i] consists of only lowercase English letters if it is non-empty.
 
 package main
+
+// O(n*m), n=number of strings, m=length of the shortest string, Space: O(1)
+func longestCommonPrefix(strs []string) string {
+	for i := 0; i < len(strs[0]); i++ {
+		for _, s := range strs {
+			if i == len(s) || s[i] != strs[0][i] {
+				return s[:i]
+			}
+		}
+	}
+	return strs[0]
+}
