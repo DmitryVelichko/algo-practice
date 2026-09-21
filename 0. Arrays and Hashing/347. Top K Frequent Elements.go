ba840876f8002882acd6f058513716383b9aba33
Dmitry Velichko <soleil-2005@mail.ru>
2026-09-21 11:25:07 +0300

    leetcode daily grind

--- 0. Arrays and Hashing/347. Top K Frequent Elements.go
+++ 0. Arrays and Hashing/347. Top K Frequent Elements.go
@@ -35,27 +35,30 @@
 
 // Bucket sort. {1: 3 times}, [[],[],[],[1]], iterate backwards
 // O(n), O(n)
-func topKFrequent(nums []int, k int) []int {
-    count := make(map[int]int)
-    freq := make([][]int, len(nums)+1)
-
-    // {1: 3 times}
-    for _, num := range nums {
-        count[num]++
-    }
-    // [[],[],[],[1]]
-    for num, cnt := range count {
-        freq[cnt] = append(freq[cnt], num)
-    }
-
-    res := []int{}
-    for i := len(freq) - 1; i > 0; i-- {
-        for _, num := range freq[i] {
-            res = append(res, num)
-            if len(res) == k { return res }
-        }
-    }
-    return res
-}
 
 package main
+
+func topKFrequent(nums []int, k int) []int {
+	count := make(map[int]int)
+	freq := make([][]int, len(nums)+1)
+
+	// {1: 3 times}
+	for _, num := range nums {
+		count[num]++
+	}
+	// [[],[],[],[1]]
+	for num, cnt := range count {
+		freq[cnt] = append(freq[cnt], num)
+	}
+
+	res := []int{}
+	for i := len(freq) - 1; i > 0; i-- {
+		for _, num := range freq[i] {
+			res = append(res, num)
+			if len(res) == k {
+				return res
+			}
+		}
+	}
+	return res
+}
