e7a83a5fa825e4acaa072b63de77457e62f75922
Dmitry Velichko <soleil-2005@mail.ru>
2026-09-22 11:48:13 +0300

    leetcode daily grind

--- 0. Arrays and Hashing/128. Longest Consecutive Sequence.go
+++ 0. Arrays and Hashing/128. Longest Consecutive Sequence.go
@@ -28,3 +28,31 @@
 // -109 <= nums[i] <= 109
 
 package main
+
+// HashSet, check with set if there's a number before and after in set
+// O(n), O(n)
+func longestConsecutive(nums []int) int {
+	numSet := make(map[int]struct{})
+	longest := 0
+	for _, num := range nums {
+		numSet[num] = struct{}{}
+	}
+
+	for num := range numSet {
+		// check if it's the start of a sequence
+		if _, found := numSet[num-1]; !found {
+			length := 1
+			for {
+				if _, exists := numSet[num+length]; exists {
+					length++
+				} else {
+					break
+				}
+			}
+			if length > longest {
+				longest = length
+			}
+		}
+	}
+	return longest
+}
